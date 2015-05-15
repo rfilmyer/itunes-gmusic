ed2d64a430acb38b825133c5abf82821b14ca8be
Roger Filmyer <roger.filmyer@gmail.com>
2015-05-15 14:01:21 -0400

    Added more edge cases to unit tests

--- unittests.py
+++ unittests.py
@@ -16,21 +16,28 @@ class LoadSampleLibraries(unittest.TestCase):
         self.assertIsInstance(self.ituneslib, list)
         self.assertIsInstance(self.ituneslib[0], dict)
         self.assertIsInstance(self.ituneslib[0]['Name'], str)
-        self.assertEqual(len(self.ituneslib), 24)
+        self.assertEqual(len(self.ituneslib), 26, msg="Wrong # of songs in iTunes library")
 
     def test_gmusic_lib_structure(self):
-        self.assertEqual(len(self.gmusiclib), 27)
+        self.assertEqual(len(self.gmusiclib), 29)
 
     def test_exact_matches(self):
-        self.assertEqual(len(self.exact_match_results['match']), 23)
+        self.assertEqual(len(self.exact_match_results['match']), 24, msg= "Wrong # of exact matches")
 
     def test_exact_mismatches_itunes(self):
-        self.assertEqual(self.exact_match_results['imismatch'], [16934])
+        imismatch = self.exact_match_results['imismatch']
+        self.assertNotIn(7400, imismatch, msg="Matched with same title/wrong artist")
+        self.assertNotIn(6526, imismatch, msg="Matched with same title/wrong artist")
 
-    def text_exact_mismatches_gmusic(self):
+        self.assertEqual(imismatch, [10534, 16934], msg= "Other matching errors, got " + str(imismatch) + ", expected [16934]")
+
+    def test_exact_mismatches_gmusic(self):
         known_mismatch_set = [u'95c66470-060f-30ec-b2b9-2965b5006f24', u'987130e2-f2e4-3cbc-887d-96835a1d9ada',
-                              u'ccab029e-1899-35a6-99bf-554f34711daf', u'24ffedbb-4588-363e-a064-ebcdd2ae63eb']
-        self.assertEqual(self.exact_match_results['gmismatch'], known_mismatch_set)
+                              u'ccab029e-1899-35a6-99bf-554f34711daf', u'24ffedbb-4588-363e-a064-ebcdd2ae63eb',
+                              u'7b362b61-8203-32b6-b97e-45bb4b4ff6e1']
+        self.assertEqual(sorted(self.exact_match_results['gmismatch']), sorted(known_mismatch_set))
 
+    def tearDown(self):
+        pass
 if __name__ == '__main__':
     unittest.main()
